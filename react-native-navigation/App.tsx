87e42849c5acf88d59feb0745e658edfa9b7db83
Francois Dungca <francoisdungca0521@gmail.com>
2026-04-17 15:04:56 +0800

    Done with initial params section

--- react-native-navigation/App.tsx
+++ react-native-navigation/App.tsx
@@ -13,9 +13,9 @@ function HomeScreen() {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
+
       <Button
         onPress={() => {
-          /* 1. Navigate to the Details route with params */
           navigation.navigate('Details', {
             itemId: 86,
             otherParam: 'anything you want here',
@@ -31,21 +31,20 @@ function HomeScreen() {
 function DetailsScreen({ route }) {
   const navigation = useNavigation();
 
-  /* 2. Get the param */
   const { itemId, otherParam } = route.params;
 
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Details Screen</Text>
+
       <Text>itemId: {JSON.stringify(itemId)}</Text>
       <Text>otherParam: {JSON.stringify(otherParam)}</Text>
+
       <Button
-        onPress={
-          () =>
-            navigation.push('Details', {
-              // Randomly generate an ID for demonstration purposes
-              itemId: Math.floor(Math.random() * 100),
-            })
+        onPress={() =>
+          navigation.push('Details', {
+            itemId: Math.floor(Math.random() * 100),
+          })
         }
       >
         Go to Details... again
@@ -57,7 +56,10 @@ function DetailsScreen({ route }) {
 const RootStack = createNativeStackNavigator({
   screens: {
     Home: HomeScreen,
-    Details: DetailsScreen,
+    Details: {
+      screen: DetailsScreen,
+      initialParams: { itemId: 42 },
+    },
   },
 });
 
